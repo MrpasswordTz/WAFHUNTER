1a25b8e091c3cf0716caf77db8faea4010e34d01
Mr.passwordTZ <104827262+MrpasswordTz@users.noreply.github.com>
2024-07-10 23:10:19 +0300

    Update wafhunter.py

--- wafhunter.py
+++ wafhunter.py
@@ -3,8 +3,8 @@ from modules import*
 def main():
     print(banner)
     host = input("Enter website name (without http/https): ")
-    result = check_waf(host)
     print(checking + host)
+    result = check_waf(host)
     print(detection)
     print("The site", Fore.RED+host+Fore.RESET + " is Behind " + Fore.YELLOW+result+Fore.RESET)
 
